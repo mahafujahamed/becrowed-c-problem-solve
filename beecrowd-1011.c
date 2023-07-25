5298a3ca1a5d0115fa4dcd65eaaf61aaf14538b9
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-07-26 01:46:16 +0600

    first commit

---
+++ beecrowd-1011.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+    #include <math.h>
+
+    int main()
+    {
+         int a;
+         scanf("%i", &a);
+         printf("VOLUME = %.3lf\n", (4 * 3.14159 * pow(a, 3)) / 3);
+         return 0;
+      }
---
+++ beecrowd-1016.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int car,distance;
+    scanf("%d",&car);
+    distance=car*2;
+    printf("%d minutos\n",distance);
+    return 0;
+}
